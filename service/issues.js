3782e91a2dbb56232759069f687308a27eecce3c
test <test>
2016-06-19 14:18:16 -0500

    adding issues service

--- service/issues.js
+++ service/issues.js
@@ -4,10 +4,15 @@
 var Issue = require('../model/issue.js');
 
 var issues = {
-    getAll: function(req, res){
-        new Issue().findAll(function(user){
-            res.json(user);
-            //next()
+    findByQuery: function(req, res){
+    	var test = req.query
+    	console.log(test)
+    	var offset = req.query.offset
+    	var key = req.query.key
+    	var param = req.query.param
+        new Issue().findByQuery(key, param, offset,function(issues){
+        	var issues = new Issue(issues)
+            res.json(issues);
         });
     }
 };
