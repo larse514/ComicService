dc495d4572451fafaf23254fc503f6c9536695e4
test <test>
2016-06-19 14:19:22 -0500

    adding constants class

---
+++ util/constants.js
@@ -0,0 +1,15 @@
+var constants = {
+	COMIC_API_RESPONSE : 
+		{
+			OK:1,
+			Invalid: 100,
+			Not_Found : 101,
+			Format_Error : 102,
+			json_callback : 103,
+			Filter_Error : 104
+			 
+		}
+
+}
+
+module.exports=constants
