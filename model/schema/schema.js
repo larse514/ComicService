b37169e101eb1885f421c3e90bfeb67e173829db
test <test>
2016-06-19 14:17:30 -0500

    adding new issue model

---
+++ model/schema/schema.js
@@ -0,0 +1,28 @@
+/**
+ * Created by andrew.larsen on 2/13/2016.
+ */
+//schema.js
+schemas = {
+    offset : null,
+    number_of_total_results: null,    
+    results:[{
+        id: null,
+        cover_date: null, 
+        description: null,
+        image: null,
+        issue_number: null,
+        name: null,
+        volume: null,
+        image:{
+            icon_url:null,
+            medium_url:null,
+            screen_url:null,
+            small_url:null,
+            super_url:null,
+            thumb_url:null,
+            tiny_url:null
+        }
+    }]
+};
+
+module.exports = schemas;
