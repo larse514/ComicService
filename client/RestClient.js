9296ae32a52fb87c6c0a1681ce01be603a001d86
test <test>
2016-06-19 14:16:50 -0500

    adding RestClient.js

--- client/RestClient.js
+++ client/RestClient.js
@@ -1,25 +1,35 @@
 var http = require('follow-redirects').http;
 var config = require('config').get('COMIC_API_PARAMS');
+var constants = require('../util/constants');
 
-var getIssue = function (query, next) {
-    http.get({
-        host: 'www.comicvine.gamespot.com',
-        path: '/api/issues/?api_key=' + config.API_KEY + '&format=json&field_list=' + config.FIELD_LIST + '&filter=' + query + '&offset=9&limit=' + config.LIMIT,
-        headers: {
-            'Content-Type': 'application/x-www-form-urlencoded',
-            'User-Agent': 'POC' }
-    }, function(response) {
-        // Continuously update stream with data
-        var body = '';
-        response.on('data', function(d) {
-            //console.log(body)
-            body += d;
+var client = { 
+    getIssues : function (query, offset, next) {
+        var path1 = '/api/issues/?api_key=' + config.API_KEY + '&format=json&field_list=' + config.FIELD_LIST + 
+                    '&filter=' + query + offset + '&limit=' + config.LIMIT;
+        http.get({
+            host: 'www.comicvine.gamespot.com',
+            path: path1,
+            headers: {
+                'Content-Type': 'application/x-www-form-urlencoded',
+                'User-Agent': 'POC' }
+        }, function(response) {
+            // Continuously update stream with data
+            var body = '';
+            response.on('data', function(d) { 
+                body += d;
+            });
+            response.on('end', function() {
+                body = JSON.parse(body)
+                if(body.status_code === constants.COMIC_API_RESPONSE.OK) {
+                    // Data reception is done, do whatever with it!
+                    next(null, body)
+                }else {
+                    console.log(body.error)
+                    console.log('error')
+                    next(body.error, null)
+                }
+            });
         });
-        response.on('end', function() {
-
-            // Data reception is done, do whatever with it!
-            console.log(body)
-            next(body)
-        });
-    });
+    }
 }
+module.exports = client
